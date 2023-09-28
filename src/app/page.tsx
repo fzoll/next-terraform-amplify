1ea360f362f8a3f9b0b7579949af220a540f0c0c
zoltan-fodi <zoltan-fodi@pluralsight.com>
2023-09-28 22:18:23 +0200

    Adding send email, just 4 testing purposes (SSR)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
@@ -38,6 +39,8 @@ export default function Home() {
           priority
         />
       </div>
+
+      <Contact />
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
